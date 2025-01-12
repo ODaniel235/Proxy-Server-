abfbb28d29f99a2ed614be42d319723ef58d4d20
Omole Oluwadamilare Daniel <domole235@gmail.com>
2025-01-12 20:29:18 +0100

    First and final push

---
+++ index.js
@@ -0,0 +1,28 @@
+import cors from "cors";
+import express from "express";
+import fetch from "node-fetch";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get("*", async (req, res) => {
+  try {
+    const { url } = req.query;
+    if (!url) {
+      return res.status(400).json({ error: "No URL provided" });
+    }
+    const response = await fetch(url);
+    console.log(response.headers);
+    if (!response.ok) {
+      return res.status(404).json({ error: "Failed to fetch file" });
+    }
+    const contentType = response.headers.get("content-type");
+    res.set("Content-Type", contentType);
+    response.body.pipe(res);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+app.listen(3000, () => {
+  console.log("Server is running on port: ", 3000);
+});
